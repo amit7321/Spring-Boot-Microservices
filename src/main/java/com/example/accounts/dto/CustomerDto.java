b9c06ecf668f1b7084b7b8f269d6c9bd17c56060
amit <amit.hasan7321@gmail.com>
2023-12-05 17:48:31 +0600

    Create dtos for the project.

---
+++ src/main/java/com/example/accounts/dto/CustomerDto.java
@@ -0,0 +1,16 @@
+package com.example.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
