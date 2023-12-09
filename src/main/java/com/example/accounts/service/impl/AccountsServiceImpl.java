f265a5add039cc48381c452017095d69459c089b
amit7321 <amit.hasan7321@gmail.com>
2023-12-10 02:17:12 +0600

    Implement create new customer and account method.

--- src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.accounts.service.impl;
 
+import com.example.accounts.constants.AccountsConstants;
 import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.entity.Accounts;
+import com.example.accounts.entity.Customer;
+import com.example.accounts.mappper.CustomerMapper;
 import com.example.accounts.repository.AccountsRepository;
 import com.example.accounts.repository.CustomerRepository;
 import com.example.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +22,21 @@ public class AccountsServiceImpl implements IAccountService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer customerSaved = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(customerSaved));
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getId());
+        long newAccountNumber = 10000000L + new Random().nextInt(90000000);
+        newAccount.setAccountNumber(newAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+
+        return newAccount;
     }
 }
+
+
