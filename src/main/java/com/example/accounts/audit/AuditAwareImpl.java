57e1ec815c394fa4a232ee723caedbc3f3b9c93a
amit <amit.hasan7321@gmail.com>
2023-12-26 18:52:27 +0600

    Add maven dependency of open-api.

--- src/main/java/com/example/accounts/audit/AuditAwareImpl.java
+++ src/main/java/com/example/accounts/audit/AuditAwareImpl.java
@@ -2,7 +2,6 @@ package com.example.accounts.audit;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
-
 import java.util.Optional;
 
 @Component("auditAwareImpl")
